ed65c8d93a265d13aefd7263addc49c6630adff7
valium69mg <carlostranquilino.cr@gmail.com>
2024-05-24 16:58:43 -0600

    Add CRUD operations for students table in database class
    1. Add CRUD operations with functions that get, set, update and delete
    2. Add a table with html to display students
    3. Add a function on Database for the search bar of the students

--- html/Database.php
+++ html/Database.php
@@ -109,6 +109,23 @@ class Database {
         }
     }
 
+    private function getUserID($username) {
+        if ($this->connectionStatus === true) {
+            try {
+                $stmt = "SELECT userid FROM users WHERE username = ?";
+                $query = $this->connection->prepare($stmt,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+                $query->execute(array($username));
+                $data = $query->fetchAll(PDO::FETCH_ASSOC);
+                return $data[0]['userid']; // returns user
+                
+            } catch (PDOException $e) {
+                return $e;
+            }
+        } else {
+            return false;
+        }
+    }
+
     public function updateUserPassword($username,$password) {
         if ($this->connectionStatus === true) {
             try {
@@ -149,6 +166,10 @@ class Database {
                 $statement = "CREATE TABLE IF NOT EXISTS students( 
                     studentid int AUTO_INCREMENT,
                     name varchar(25) NOT NULL,
+                    p1 int NOT NULL DEFAULT 0,
+                    p2 int NOT NULL DEFAULT 0,
+                    p3 int NOT NULL DEFAULT 0,
+                    cf int NOT NULL DEFAULT 0,
                     userid int,
                     PRIMARY KEY (studentid),
                     FOREIGN KEY (userid) REFERENCES users(userid)
@@ -162,5 +183,99 @@ class Database {
             return false;
         }
     }
+
+    public function createStudent($username,$name){
+        $userid = $this->getUserID($username);
+        if ($this->connectionStatus === true) {
+            try {
+                $stmt = "INSERT INTO students (name,userid) VALUES (?,?)";
+                $query = $this->connection->prepare($stmt,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+                $query->execute((array($name,$userid)));
+                return true;
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function getAllStudents($username) {
+        $userid = $this->getUserID($username);
+        if ($this->connectionStatus === true) {
+            try {
+                $stmt = "SELECT * FROM students WHERE userid = ?";
+                $query = $this->connection->prepare($stmt,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+                $query->execute((array($userid)));
+                return $query->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function getStudent($studentid) {
+        if ($this->connectionStatus === true) {
+            try {
+                $stmt = "SELECT * FROM students WHERE studentid = ?";
+                $query = $this->connection->prepare($stmt,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+                $query->execute((array($studentid)));
+                return $query->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function searchStudent($studentname) {
+        if ($this->connectionStatus === true) {
+            try {
+                $stmt = "SELECT * FROM students WHERE name LIKE ?";
+                $query = $this->connection->prepare($stmt,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+                $query->execute((array('%' . $studentname . '%')));
+                return $query->fetch(PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function updateStudentGrades($studentid,$p1=0,$p2=0,$p3=0,$cf=0) {
+        if ($this->connectionStatus === true) {
+            try {
+                $stmt = "UPDATE students SET p1 = ?, p2 = ?, p3 = ?, cf = ? WHERE studentid = ?";
+                $query = $this->connection->prepare($stmt,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+                $query->execute((array($p1,$p2,$p3,$cf,$studentid)));
+                return true;
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function deleteStudent( $studentid ) {
+        if ($this->connectionStatus === true) {
+            try {
+                $stmt = "DELETE FROM students WHERE studentid = ?";
+                $query = $this->connection->prepare($stmt,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+                $query->execute((array($studentid)));
+                return true;
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        } else {
+            return false;
+        }
+    }
+
+
 }
 
--- sql_querys/students.sql
+++ sql_querys/students.sql
@@ -1,6 +1,10 @@
 CREATE TABLE IF NOT EXISTS students( 
     studentid int AUTO_INCREMENT,
     name varchar(25) NOT NULL,
+    p1 int NOT NULL DEFAULT 0,
+    p2 int NOT NULL DEFAULT 0,
+    p3 int NOT NULL DEFAULT 0,
+    cf int NOT NULL DEFAULT 0,
     userid int,
     PRIMARY KEY (studentid),
     FOREIGN KEY (userid) REFERENCES users(userid)
